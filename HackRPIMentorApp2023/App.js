8bc29224e7465a262f01d943fc504025e2488618
CharlieC133 <69218910+CharlieC133@users.noreply.github.com>
2023-09-25 00:46:33 -0400

    Update App.js

--- HackRPIMentorApp2023/App.js
+++ HackRPIMentorApp2023/App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import{}from 'firebase/firestore';
+import{} from ' firebase/app'
+import{} from 'firebase/auth'
+import{} from 'firebase/analytics'
 export default function App() {
   return (
     <View style={styles.container}>
