13b4503772b2abe3e09730d4279fb7056fb27b50
Yu Qing Peng <132966295+yuqingp0131@users.noreply.github.com>
2023-09-26 16:53:04 -0400

    test

---
+++ HackRPIEventApp2023/calendar/ScrollableBox.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+
+const ScrollableBox = () => {
+  return (
+    // <div className="scrollable-box">
+    //   <div className="inner-content">
+    //     {/* Add your content here */}
+    //   </div>
+    // </div>
+    <Text>Success!</Text>
+  );
+};
+
+export default ScrollableBox;
