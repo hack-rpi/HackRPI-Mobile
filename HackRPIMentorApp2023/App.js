aeedec75d8aab387c6cea3f4500c93a60255237b
in2yi <108951060+in2yi@users.noreply.github.com>
2023-09-26 17:11:00 -0400

    same as last time but added specifics for out firebase

--- HackRPIMentorApp2023/App.js
+++ HackRPIMentorApp2023/App.js
@@ -1,21 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import * as firebase from "firebase/app";
-import 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAqGfFX7gXRGBtidctQjIJ4NC0FA6YxeOQ",
-    authDomain: "removed for post",
-    databaseURL: "removed for post",
-    projectId: "goalsdev-7eb67",
-    storageBucket: "goalsdev-7eb67.appspot.com",
-    messagingSenderId: "362368452051",
-    appId: "removed for post",
-    measurementId: "G-CNRGY3FTLE"
+  apiKey: "AIzaSyAqGfFX7gXRGBtidctQjIJ4NC0FA6YxeOQ",
+  authDomain: "mentor-queue-c01a3.firebaseapp.com",
+  projectId: "mentor-queue-c01a3",
+  storageBucket: "mentor-queue-c01a3.appspot.com",
+  messagingSenderId: "117425105410",
+  appId: "1:117425105410:web:60fa2b3e348b489b37551b",
+  measurementId: "G-NJ5ZBXKBX3"
 };
 
-firebase.initializeApp(firebaseConfig);
-
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export default function App() {
   return (
     <View style={styles.container}>
