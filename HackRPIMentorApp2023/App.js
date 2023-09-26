9de5e7a23e802d6e68f4bd07f849b4143d11e756
CharlieC133 <69218910+CharlieC133@users.noreply.github.com>
2023-09-26 16:24:50 -0400

    added imports

---
+++ HackRPIMentorApp2023/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import{}from 'firebase/firestore';
+import{} from ' firebase/app'
+import{} from 'firebase/auth'
+import{} from 'firebase/analytics'
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
