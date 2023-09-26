07598fc4241cac5e72211941b4a77394c5aa0bde
kesfc <kevinli20030305@gmail.com>
2023-09-26 17:07:54 -0400

    test

---
+++ HackRPIEventApp2023/Calander/Calander.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native'
+
+function Calander() {
+  return (
+    <View>
+      <Text>My React Native Component</Text>
+    </View>
+  );
+}
+
+export default Calander;
