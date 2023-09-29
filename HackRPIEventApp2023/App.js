2ff7e626e836455af0137496a27d278af7e60346
jxia20 <113316739+jxia20@users.noreply.github.com>
2023-09-29 16:45:26 -0400

    Added two circles

--- HackRPIEventApp2023/App.js
+++ HackRPIEventApp2023/App.js
@@ -1,15 +1,26 @@
-import { useState} from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Timer from './components/Timer';
+
+const Circle = ({ size, percentage }) => {
+  return (
+    <View style={[styles.circle, { width: size, height: size }]}>
+      <Timer percentage={percentage} circleWidth={size.toString()} />
+    </View>
+  );
+};
 
 export default function App() {
   const [percentage, setPercentage] = useState(35);
-  // Any constants are from tutorial; will be changed to dynamic later
+
   return (
     <View style={styles.container}>
-      <Timer percenatage={percentage} circleWidth="200" />
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.circleContainer}>
+        <Circle size={100} percentage={percentage} />
+        <Timer percentage={percentage} circleWidth="200" />
+        <Circle size={100} percentage={percentage} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,5 +32,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  circleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  circle: {
+    borderRadius: 50,
+    overflow: 'hidden',
   },
 });
