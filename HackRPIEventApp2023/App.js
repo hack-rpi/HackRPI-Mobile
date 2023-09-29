e3e33e34aee31987f09cdff9ed96d21d43fae776
jxia20 <113316739+jxia20@users.noreply.github.com>
2023-09-29 17:08:52 -0400

    added the countdown

--- HackRPIEventApp2023/App.js
+++ HackRPIEventApp2023/App.js
@@ -1,26 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import Timer from './components/Timer';
+import { StyleSheet, View, Text } from 'react-native';
 
-const Circle = ({ size, percentage }) => {
+const Timer = ({ percentage, circleWidth, circleHeight, timeUnit, timeRemaining }) => {
+  const circleSize = { width: circleWidth, height: circleHeight };
+
   return (
-    <View style={[styles.circle, { width: size, height: size }]}>
-      <Timer percentage={percentage} circleWidth={size.toString()} />
+    <View style={[circleSize, styles.circle]}>
+      <Text style={styles.timerText}>{timeRemaining}</Text>
     </View>
   );
 };
 
+
 export default function App() {
-  const [percentage, setPercentage] = useState(35);
+  const totalSeconds = 6048000; // 1 hour in seconds
+  const [secondsRemaining, setSecondsRemaining] = useState(totalSeconds);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSecondsRemaining(prevSeconds => {
+        if (prevSeconds > 0) {
+          return prevSeconds - 1;
+        } else {
+          clearInterval(interval); // Clear the interval when countdown reaches 0
+          return 0;
+        }
+      });
+    }, 1000);
+
+    // Clear the interval when the component is unmounted
+    return () => clearInterval(interval);
+  }, []);
+
+  const days = Math.floor(secondsRemaining / (3600 * 24));
+  const hours = Math.floor((secondsRemaining % (3600 * 24)) / 3600);
+  const minutes = Math.floor((secondsRemaining % 3600) / 60);
+  const remainingSeconds = secondsRemaining % 60;
 
   return (
     <View style={styles.container}>
       <View style={styles.circleContainer}>
-        <Timer percentage={percentage} circleWidth="200" />
-        <Timer percentage={percentage} circleWidth="200" />
-        <Timer percentage={percentage} circleWidth="200" />
-        <Timer percentage={percentage} circleWidth="200" />
+        <Timer
+          percentage={((totalSeconds - secondsRemaining) / totalSeconds) * 100}
+          circleWidth="200"
+          circleHeight="200"
+          timeUnit="days"
+          timeRemaining={days}
+        />
+        <Timer
+          percentage={((totalSeconds - secondsRemaining) / totalSeconds) * 100}
+          circleWidth="200"
+          circleHeight="200"
+          timeUnit="hours"
+          timeRemaining={hours}
+        />
+        <Timer
+          percentage={((totalSeconds - secondsRemaining) / totalSeconds) * 100}
+          circleWidth="200"
+          circleHeight="200"
+          timeUnit="minutes"
+          timeRemaining={minutes}
+        />
+        <Timer
+          percentage={((totalSeconds - secondsRemaining) / totalSeconds) * 100}
+          circleWidth="200"
+          circleHeight="200"
+          timeUnit="seconds"
+          timeRemaining={remainingSeconds}
+        />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -32,8 +80,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'flex-start', // Align items at the start of the container (top of the screen)
-    paddingTop: 50, // Add padding to the top of the container to move circles away from the top
+    justifyContent: 'flex-start',
+    paddingTop: 50,
   },
   circleContainer: {
     flexDirection: 'row',
@@ -41,7 +89,18 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
   },
   circle: {
-    borderRadius: 50,
-    overflow: 'hidden',
+    width: '100%',
+    height: '100%',
+    borderRadius: 100 / 2,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'red', // Border color for the circle
+  },
+  timerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
