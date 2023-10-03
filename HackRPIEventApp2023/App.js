619a4dc41e5fa7d921cc43ccbd6561187285d07e
Abby Itty <73668351+abigailitty@users.noreply.github.com>
2023-10-03 17:40:13 -0400

    Cleaned a bit
    trying to figure out how to get floating boxes :O

--- HackRPIEventApp2023/App.js
+++ HackRPIEventApp2023/App.js
@@ -1,27 +1,29 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      backgroundColor: '#191919', //the name of this must be background color!!
+      mainRed: '#910307' ,
+      secondaryGray: '#9E9E9E' , //reguar back and white are cnstant anyway
+      alignItems: 'center',
+    },
+    
+  });
+  
   return (
-    <View
-      style={[
-        styles.container,
-        {
-          // Try setting `flexDirection` to `"row"`.
-          flexDirection: 'column',
-        },
-      ]}> //To Do: add cmpnets!
-      <View style={{flex: 1, backgroundColor: styles.backgroundBlack}} /> //fix this!!
-      <View style={{flex: 2, backgroundColor: styles.mainRed}} />
-      <View style={{flex: 3, backgroundColor: styles.secondaryGray}} />
+    <View style={[styles.container]}>
+      <View style={{flex: 0.25, backgroundColor: '#9E9E9E', flexDirection: 'row'}} ><Text>Hi</Text></View>
+      <View style={{flex: 0.25, backgroundColor: styles.container.mainRed}} ></View>
+      <View style={{flex: 0.25, backgroundColor: styles.secondaryGray}} ></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-  },
-});
+
