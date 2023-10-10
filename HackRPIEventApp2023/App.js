7bc0f9b09d0b3851d37f606698f5baa864eeb7e6
Abby Itty <73668351+abigailitty@users.noreply.github.com>
2023-10-10 16:33:50 -0400

    Added boxes!
    added event boxes, left space at the bottom for a nav bar
    
    gotta fix scrollview so that boxes can be wider

--- HackRPIEventApp2023/App.js
+++ HackRPIEventApp2023/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
 
@@ -13,15 +13,41 @@ export default function App() {
       mainRed: '#910307' ,
       secondaryGray: '#9E9E9E' , //reguar back and white are cnstant anyway
       alignItems: 'center',
+      marginBottom: 49,
     },
     
+    floatingBox: {
+      //flex: 1,
+      //flexDirection: 'column'
+      //^^ no bc then the floatingBox requires text inside of it and changes to fit the text width
+
+      width: '80%',
+      height: '15%',
+      justifyContent: 'center',
+      backgroundColor: '#9E9E9E', //the name of this must be background color!!
+      alignItems: 'center',
+      margin: 15,
+      borderRadius: 10
+    },
+
+    scrollContainer: {
+      flexGrow: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '80%',
+    },
+
   });
   
   return (
     <View style={[styles.container]}>
-      <View style={{flex: 0.25, backgroundColor: '#9E9E9E', flexDirection: 'row'}} ><Text>Hi</Text></View>
-      <View style={{flex: 0.25, backgroundColor: styles.container.mainRed}} ></View>
-      <View style={{flex: 0.25, backgroundColor: styles.secondaryGray}} ></View>
+       <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={[styles.floatingBox]} ><Text>Input text here about Event 1</Text></View>
+        <View style={[styles.floatingBox]} ><Text>Input text here about Event 2</Text></View>
+        <View style={[styles.floatingBox]} ><Text>Input text here about Event 3</Text></View>
+        <View style={[styles.floatingBox]} ><Text>Input text here about Event 4</Text></View>
+        <View style={[styles.floatingBox]} ><Text>Input text here about Event 5</Text></View>
+      </ScrollView>
     </View>
   );
 }
