af3590c3f6ae33977c75069938cb5ee8028310be
Adam Smart <adam.smart830@gmail.com>
2023-10-13 17:29:12 -0400

    Fix Missing File Error

--- HackRPIEventApp2023/App.js
+++ HackRPIEventApp2023/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import MentorFrontend from "./MentorFrontend.js"
+import MentorFrontend from "./Box/MentorFrontend.js"
 
 export default function App() {
   return (
