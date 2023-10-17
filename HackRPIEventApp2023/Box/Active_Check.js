e286dec7a3508ffe8ae7c48f54a8cb6d1b741a6e
kesfc <kevinli20030305@gmail.com>
2023-10-17 15:17:12 -0400

    Added function to check if the workshop is active,
    Added function to check if the the workshop is on-going, and added the json file make it be able to compare

---
+++ HackRPIEventApp2023/Box/Active_Check.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+function Active_Check(date, StartTime, EndTime) {
+    const [isInTimeRange, setIsInTimeRange] = useState(false);
+
+    const compareTimeRange = () => {
+        const currentDateTime = new Date();
+        // Get the current date in ISO 8601 format
+        const currentDate = currentDateTime.toISOString().split('T')[0];
+        const currentTime = currentDateTime.toLocaleTimeString([], 
+            { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+        if (currentDate === date && currentTime >= StartTime && currentTime <= EndTime) {
+        setIsInTimeRange(true);
+        } else {
+        setIsInTimeRange(false);
+        }
+  };
+    useEffect(() => {
+        compareTimeRange();
+        const interval = setInterval(() => {
+            compareTimeRange();
+        }, 10000);
+        return () => clearInterval(interval);
+    }, [date, StartTime, EndTime]);
+    return isInTimeRange;
+}
+
+export default Active_Check;
--- HackRPIEventApp2023/Box/Calander.js
+++ HackRPIEventApp2023/Box/Calander.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React  from "react";
 import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
 import CalanderObject from "./CalanderObject";
 import WorkShops from "./WorkShops.json";
+import Active_Check from "./Active_Check";
+
 
 // calander object that holds all events and handles which are visable, which are active, and which are upcoming.
 const Calander = () => {
+
   return (
     <SafeAreaView style={styles.CalanderStyle}>
       <ScrollView
@@ -13,15 +16,21 @@ const Calander = () => {
         //pagingEnabled={true} // Enable paging
         //snapToInterval={150} // Set the interval to the height of a single rectangle
       >
-      {WorkShops.map((event, index) => (
-        <CalanderObject
-          key={index}
-          workshop_Title={event.WorkShop_Title}
-          Time={event.WorkShop_Time}
-          Location={event.WorkShop_Location}
-          Description={event.WorkShop_Description}
-        />
-      ))}
+      {WorkShops.map((event, index) => {
+        const Active = Active_Check(event.WorkShop_Date, event.WorkShop_StartTime, 
+        event.WorkShop_EndTime);
+        return (
+          <CalanderObject
+            key={index}
+            workshop_Title={event.WorkShop_Title}
+            Time={event.WorkShop_Date + ": " + event.WorkShop_StartTime + " - " + 
+            event.WorkShop_EndTime}
+            Location={event.WorkShop_Location}
+            Description={event.WorkShop_Description}
+            isRed={Active}
+          />
+        );
+      })}
       </ScrollView>
     </SafeAreaView>
   );
