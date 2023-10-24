828f8cc90a5427f5e739dbdd521ef1f74fe46770
Riya Singh <caspriya9211@gmail.com>
2023-10-24 16:27:19 -0400

    Added the push notification code
    It is broken

--- HackRPIEventApp2023/App.js
+++ HackRPIEventApp2023/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect }  from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Feather } from '@expo/vector-icons';
 import { StatusBar } from "expo-status-bar";
 import Calander from "./Box/Calander";
+
+import messaging from '@react-native-firebase/messaging';
 
 const Tab = createBottomTabNavigator();
 
@@ -35,6 +37,41 @@ function QueueScreen() {
 }
 
 export default function App() {
+  useEffect(() => {
+    // Request permissions and get FCM token
+    const requestUserPermission = async () => {
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        const fcmToken = await messaging().getToken();
+        console.log('FCM Token:', fcmToken);
+        // Send this token to your server to associate with the user
+      }
+    };
+
+    requestUserPermission();
+
+ // Handle incoming notifications when the app is in foreground
+  const unsubscribeOnMessage = messaging().onMessage(async remoteMessage => {
+    console.log('Foreground Notification:', remoteMessage);
+    // Handle the notification here (e.g., show an alert, update state)
+  });
+
+  // Handle notification tap when the app is in background or quit
+  const unsubscribeOnNotificationOpenedApp = messaging().onNotificationOpenedApp(remoteMessage => {
+    console.log('Notification Tapped:', remoteMessage);
+    // Handle the notification tap here (e.g., navigate to a specific screen)
+  });
+
+  // Clean up event listeners when component unmounts
+  return () => {
+  unsubscribeOnMessage();
+  unsubscribeOnNotificationOpenedApp();
+  };
+  }, []);
   return (
     <NavigationContainer>
       <Tab.Navigator
