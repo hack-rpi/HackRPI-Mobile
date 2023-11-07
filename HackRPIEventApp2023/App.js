6665ffb1ec55dc81a541b0f331b629f227a12afd
RavenLLevitt <ravenlukelevitt@gmail.com>
2023-11-07 17:22:11 -0500

    Fixing Import post merge

--- HackRPIEventApp2023/App.js
+++ HackRPIEventApp2023/App.js
@@ -1,9 +1,9 @@
-import CountdownTimer from './components/CountdownTimer';
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Feather } from '@expo/vector-icons';
+import CountdownTimer from "./Components/CountdownTimer";
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
 import Calander from "./Box/Calander";
 
@@ -12,7 +12,7 @@ const Tab = createBottomTabNavigator();
 function InfoScreen() {
   return (
     <View style={styles.container}>
-   <Calander/>
+      <Calander />
     </View>
   );
 }
@@ -21,7 +21,7 @@ function HomeScreen() {
   return (
     <View style={styles.container}>
       <CountdownTimer />
-    <Feather name="user" size={24} color="white" />
+      <Feather name="user" size={24} color="white" />
       <Text style={styles.text}>No one home gotcha! haha</Text>
     </View>
   );
@@ -44,26 +44,31 @@ export default function App() {
           tabBarIcon: ({ focused, color, size }) => {
             //adding icons
             let iconName;
-            if (route.name === 'Info') {
-              iconName = 'info';
-            } else if (route.name === 'Home') {
-              iconName = 'home';
-            } else if (route.name === 'Queue') {
-              iconName = 'user';
+            if (route.name === "Info") {
+              iconName = "info";
+            } else if (route.name === "Home") {
+              iconName = "home";
+            } else if (route.name === "Queue") {
+              iconName = "user";
             }
-            return <Feather name={iconName} size={size} color={focused ? 'red' : 'white'} />;
+            return (
+              <Feather
+                name={iconName}
+                size={size}
+                color={focused ? "red" : "white"}
+              />
+            );
           },
           tabBarLabelStyle: {
             fontSize: 12,
           },
           tabBarStyle: {
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
             borderTopWidth: 0,
           },
-          tabBarActiveTintColor: '#910307',
-          tabBarInactiveTintColor: 'white',
-        })}
-      >
+          tabBarActiveTintColor: "#910307",
+          tabBarInactiveTintColor: "white",
+        })}>
         <Tab.Screen name="Info" component={InfoScreen} />
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Queue" component={QueueScreen} />
@@ -75,11 +80,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#191919',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#191919",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    color: 'white',
+    color: "white",
   },
 });
