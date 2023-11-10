ac3cd1001eb586989673c66855d600de68aed052
jxia20 <113316739+jxia20@users.noreply.github.com>
2023-11-10 15:22:50 -0500

    added more interactive capabilities within the prizes page by using buttons and flatlists.

--- HackRPIEventApp2023/Box/prizes.js
+++ HackRPIEventApp2023/Box/prizes.js
@@ -1,18 +1,40 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Modal, FlatList, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, Modal, FlatList, StyleSheet, Animated } from 'react-native';
 
 const data = [
-  { category: 'Best Mobile App', prize: '$1000' },
-  { category: 'Best Web App', prize: '$800' },
-  { category: 'Best UI/UX Design', prize: '$500' },
+  { category: 'Best Mobile App', prize: '$1000', details: 'Details about the Best Mobile App category.' },
+  { category: 'Best Web App', prize: '$800', details: 'Details about the Best Web App category.' },
+  { category: 'Best UI/UX Design', prize: '$500', details: 'Details about the Best UI/UX Design category.' },
   // Add more categories and prizes as needed
 ];
 
 const HackathonScreen = () => {
   const [isModalVisible, setModalVisible] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const modalOpacity = new Animated.Value(0);
 
   const toggleModal = () => {
-    setModalVisible(!isModalVisible);
+    if (isModalVisible) {
+      Animated.timing(modalOpacity, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: false,
+      }).start(() => {
+        setModalVisible(false);
+        setSelectedCategory(null);
+      });
+    } else {
+      setSelectedCategory(null);
+      Animated.timing(modalOpacity, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: false,
+      }).start(() => setModalVisible(true));
+    }
+  };
+
+  const selectCategory = (category) => {
+    setSelectedCategory(category);
   };
 
   return (
@@ -20,18 +42,29 @@ const HackathonScreen = () => {
       <TouchableOpacity onPress={toggleModal} style={styles.button}>
         <Text style={styles.buttonText}>View Categories and Prizes</Text>
       </TouchableOpacity>
-      <Modal isVisible={isModalVisible}>
+      <Modal transparent visible={isModalVisible} animationType="slide">
         <View style={styles.modalContainer}>
-          <FlatList
-            data={data}
-            renderItem={({ item }) => (
-              <View style={styles.modalItem}>
-                <Text style={styles.category}>{item.category}</Text>
-                <Text style={styles.prize}>{item.prize}</Text>
-              </View>
-            )}
-            keyExtractor={(item) => item.category}
-          />
+          {selectedCategory ? (
+            <Animated.View style={[styles.modalContent, { opacity: modalOpacity }]}>
+              <Text style={styles.selectedCategory}>{selectedCategory}</Text>
+              <Text style={styles.details}>{data.find((item) => item.category === selectedCategory).details}</Text>
+            </Animated.View>
+          ) : (
+            <FlatList
+                data={data}
+                renderItem={({ item }) => (
+                  <TouchableOpacity
+                    onPress={() => selectCategory(item.category)}
+                    style={styles.modalItem}
+                  >
+                    <Text style={styles.category}>{item.category}</Text>
+                    <Text style={styles.prize}>{item.prize}</Text>
+                  </TouchableOpacity>
+                )}
+                keyExtractor={(item) => item.category}
+                contentContainerStyle={styles.flatListContainer}
+              />
+          )}
           <TouchableOpacity onPress={toggleModal} style={styles.closeButton}>
             <Text style={styles.closeButtonText}>Close</Text>
           </TouchableOpacity>
@@ -46,6 +79,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#f0f0f0',
   },
   button: {
     backgroundColor: 'blue',
@@ -57,14 +91,27 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: 'white',
     padding: 20,
     borderRadius: 10,
+    width: '80%',
   },
   modalItem: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginBottom: 10,
+    backgroundColor: 'white',
+    padding: 15,
+    borderRadius: 8,
   },
   category: {
     fontSize: 16,
@@ -73,11 +120,26 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
   },
+  flatListContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    marginTop: 50,
+  },
+  innerModalContent: {
+    alignItems: 'center',
+  },
+  selectedCategory: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  details: {
+    fontSize: 16,
+  },
   closeButton: {
     backgroundColor: 'blue',
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 20,
+    padding: 15,
+    borderRadius: 8,
   },
   closeButtonText: {
     color: 'white',
