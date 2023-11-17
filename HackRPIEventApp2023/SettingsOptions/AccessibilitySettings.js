a29a355d643f76a993a5252a50f16ccb3e355534
Riya Singh <caspriya9211@gmail.com>
2023-11-17 16:30:09 -0500

    Added colorblind mode to accessibilities page

--- HackRPIEventApp2023/SettingsOptions/AccessibilitySettings.js
+++ HackRPIEventApp2023/SettingsOptions/AccessibilitySettings.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const AccessibilityOptions = () => {
+  const [colorBlindMode, setColorBlindMode] = useState(false);
+
+  const toggleColorBlindMode = () => {
+    setColorBlindMode(!colorBlindMode);
+    // Implement functionality to apply color blind mode
+    // For example, you might apply a different color scheme
+    // or modify colors in your app based on the colorBlindMode state
+  };
+
   return (
     <View style={styles.container}>
       <Text>Accessibility Options</Text>
-      {/* Suboptions and functionality here */}
+      <TouchableOpacity onPress={toggleColorBlindMode}>
+        <Text style={styles.option}>
+          Color Blind Mode: {colorBlindMode ? 'On' : 'Off'}
+        </Text>
+      </TouchableOpacity>
+      {/* Other suboptions and functionality here */}
     </View>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +31,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  option: {
+    fontSize: 16,
+    marginTop: 10,
+    color: '#333', // You can adjust the color as needed
+  },
 });
+
+export default AccessibilityOptions;
