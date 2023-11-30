c84b68cbba978624a1c1a2aec7359bc4f7400373
Riya Singh <caspriya9211@gmail.com>
2023-11-30 15:14:06 -0500

    centered the options, changed the color of the background + the options and added "arrow-right"
    trying to make it look like the settings of an iphone

--- HackRPIEventApp2023/SettingsOptions/Settings.js
+++ HackRPIEventApp2023/SettingsOptions/Settings.js
@@ -1,29 +1,54 @@
 import React from 'react';
 import { View, Button, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 const SettingsPage = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Button
-        title="Accessibility Settings"
-        onPress={() => navigation.navigate('AccessibilitySettings')}
-      />
-      <Button
-        title="Profile Customization"
-        onPress={() => navigation.navigate('ProfileCustomization')}
-      />
-      <Button
-        title="Language Settings"
-        onPress={() => navigation.navigate('LanguageSettings')}
-      />
-      <Button
-        title="Report a Bug "
-        onPress={() => navigation.navigate('Report')}
-      />
-      <Button
-        title="Notification Settings"
-        onPress={() => navigation.navigate('NotificationSettings')}
-      />
+       <View style={styles.buttonContainer}>
+        <Button
+          title="Accessibility Settings"
+          color = "white"
+          onPress={() => navigation.navigate('AccessibilitySettings')}
+        />
+        <Feather name="arrow-right" size={24} color="white" style={styles.icon} />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Profile Customization"
+          color = "white"
+          onPress={() => navigation.navigate('ProfileCustomization')}
+        />
+        <Feather name="arrow-right" size={24} color="white" style={styles.icon} />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Language Settings"
+          color = "white"
+          onPress={() => navigation.navigate('LanguageSettings')}
+        />
+        <Feather name="arrow-right" size={24} color="white" style={styles.icon} />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Report a Bug "
+          color = "white"
+          onPress={() => navigation.navigate('Report')}
+        />
+        <Feather name="arrow-right" size={24} color="white" style={styles.icon} />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Notification Settings"
+          color = "white"
+          onPress={() => navigation.navigate('NotificationSettings')}
+        />
+        <Feather name="arrow-right" size={24} color="white" style={styles.icon} />
+      </View>
     </View>
   );
 };
@@ -32,10 +57,18 @@ const SettingsPage = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    backgroundColor: '#191919',
     padding: 20,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  icon: {
+    marginLeft: 10,
   },
 });
 
