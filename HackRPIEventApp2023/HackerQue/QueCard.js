6fac8288058be70dfa7ec36ce7e3baac538d97dd
Yu Qing Peng <132966295+yuqingp0131@users.noreply.github.com>
2023-12-03 22:11:12 -0500

    Update mentor queue page

--- HackRPIEventApp2023/HackerQue/QueCard.js
+++ HackRPIEventApp2023/HackerQue/QueCard.js
@@ -6,7 +6,8 @@ import { useState, useEffect } from "react";
 
 function QueCard() {
   const Start_Time = "2023-11-10T10:00:00Z";
-  const Position = 20;
+  // ^^^ the time above is hardcode, replace it with  the time student enter the queue
+  const Position = 80;
   const Total = 100;
   const [Time_In_Queue, setTimeInQueueHours] = useState(
     calculateTimeDifference(Start_Time)
@@ -28,23 +29,33 @@ function QueCard() {
       <View style={styles.heading}>
         <Text style={styles.textHeader}>Issue: HTML</Text>
       </View>
+
       <View style={styles.row}>
-        <View style={styles.subBox}>
+        <View style={styles.subBox1}>
           <CircleProgress
             percentage={(Position / Total) * 100}
-            circleWidth={130}
-          />
+            circleWidth={150}/>
         </View>
-        <View style={styles.subBox}>
-          <Text style={styles.textHeader}> TIQ: {Time_In_Queue}</Text>
-        </View>
+
+        <View style={styles.subBox2}>
+          <View style={styles.tiq_box}>
+            <Text style={[styles.tiq_text]}>Time in Queue</Text>
+          </View>
+          <Text style={[styles.time]}>01:20:38</Text>  
+          {/*The time is hardcode, replace the line above with this line if the start time is known :
+          --->  <Text style={[styles.textHeader, { textAlign: 'center', lineHeight: 24 }]}>{Time_In_Queue}</Text> */}
+          </View>
       </View>
+
+
       <View style={styles.row}>
-        <View style={styles.subBox}>
-          <Text style={styles.textBody}> </Text>
+        <View style={styles.subBox3}>
+          <Text style={styles.textBody}>Position: {Position} </Text>
         </View>
-        <View style={styles.subBox}>
-          <Text style={styles.textBody}> Position: {Position}</Text>
+        <View style={styles.subBox4}>
+          <View style={styles.pause_box}>
+            <Text style={[styles.pause_text]}>Pause</Text>
+          </View>
         </View>
       </View>
     </View>
@@ -64,13 +75,37 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     width: 300,
   },
-  subBox: {
-    width: 150,
-    height: 200,
+  subBox1: {
+    width: 130,
+    height: 100,
+    alignContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 100,
+  },
+  subBox2: {
+    width: 200,
+    height: 130,
+    alignContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 70,
+  },
+  subBox3: {
+    width: 170,
+    height: 100,
     alignContent: "center",
     alignItems: "center",
     justifyContent: "center",
   },
+  subBox4: {
+    width: 150,
+    height: 100,
+    alignContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
   heading: {
     width: 300,
     height: 50,
@@ -83,9 +118,9 @@ const styles = StyleSheet.create({
   },
   circleTemp: {
     width: 130,
-    height: 130,
+    height: 100,
     borderRadius: 100,
-    margin: 20,
+    margin: 10,
     backgroundColor: "#FFFFFF",
   },
   textHeader: {
@@ -95,8 +130,56 @@ const styles = StyleSheet.create({
   },
   textBody: {
     color: "#ffffff",
-    fontSize: 20,
+    fontSize: 15,
   },
+  tiq_box:{
+    width: 150,
+    height: 32,
+    backgroundColor: "white",
+    justifyContent: "center", // To vertically center the text
+    alignItems: "center", // To horizontally center the text
+    borderRadius: 8,
+    borderWidth: 3,
+    
+  },
+  tiq_text:{
+    color:"#910307",
+    textAlign: 'center',
+    fontSize: 17,
+    fontWeight: 600,
+    lineHeight: 24,
+  },
+  time:{
+    color: "#ffffff",
+    textAlign: 'center',
+    marginTop: 10,
+    fontSize: 25,
+    letterSpacing: 1,
+    lineHeight: 24,
+    marginTop: 20,
+    fontWeight: 400,
+  },
+  pause_box:{
+    width: 100,
+    height: 32,
+    backgroundColor: "#910307",
+    justifyContent: "center", // To vertically center the text
+    alignItems: "center", // To horizontally center the text
+    borderRadius: 8,
+    borderWidth: 3,
+    
+  },
+
+  pause_text:{
+    color:"white",
+    textAlign: 'center',
+    fontSize: 17,
+    fontWeight: 600,
+    lineHeight: 24,
+  },
+
+
+
 });
 
 export default QueCard;
