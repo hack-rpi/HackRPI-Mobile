2527f7258bf3c38d3e61faae6e08e974c42f0608
Adam Smart <adam.smart830@gmail.com>
2023-12-03 23:10:54 -0500

    Minor bu fix

--- HackRPIEventApp2023/App.js
+++ HackRPIEventApp2023/App.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Feather } from '@expo/vector-icons';
-import { StatusBar } from "expo-status-bar";
-import Calander from "./Box/Calander";
-import Settings from "./SettingsOptions/Settings"
 import { createStackNavigator } from '@react-navigation/stack';
-import AccessibilitySettingsScreen from "./SettingsOptions/AccessibilitySettings"
+import { Feather } from '@expo/vector-icons';
+import Calander from "./Box/Calander";
+import Settings from "./SettingsOptions/Settings";
+import { StatusBar } from "expo-status-bar";
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -15,7 +14,7 @@ const Stack = createStackNavigator();
 function InfoScreen() {
   return (
     <View style={styles.container}>
-   <Calander/>
+      <Calander />
     </View>
   );
 }
@@ -23,7 +22,7 @@ function InfoScreen() {
 function HomeScreen() {
   return (
     <View style={styles.container}>
-    <Feather name="user" size={24} color="white" />
+      <Feather name="user" size={24} color="white" />
       <Text style={styles.text}>No one home gotcha! haha</Text>
     </View>
   );
@@ -38,45 +37,18 @@ function QueueScreen() {
   );
 }
 
-function SettingsScreen() {
+function SettingsStackNavigator() {
   return (
-    <View style={styles.container}>
-      <Feather name="settings" size={24} color="white" />
-      <Settings/>
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Settings"
+        component={Settings}
+        options={{ headerShown: false }}
+      />
+      {/* Add more screens for settings here if needed */}
+    </Stack.Navigator>
   );
 }
-
-/**SettingsStackNavigator is a separate function that defines a stack navigator (StackNavigator) 
- * managing screens related to settings (SettingsPage in this case). Within the SettingsScreen component, 
- * the SettingsStackNavigator is included to handle navigation within the Settings tab. */
-//NEED TO BE FIXED- Cauisng problems
-// function SettingsStackNavigator() {
-//   return (
-//     <Stack.Navigator>
-//       <Stack.Screen
-//         name="Settings"
-//         component={SettingsScreen}
-//         options={{
-//           headerShown: false // If you want to hide the header for this stack
-//         }}
-//       />
-//       {/* Add more screens within this Stack.Navigator if needed */}
-//     </Stack.Navigator>
-//   );
-// }
-
-// function SettingsScreen() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       <Feather name="settings" size={24} color="black" />
-//       <Text><Settings/></Text>
-//       {/* Include the SettingsStackNavigator here */}
-//       <SettingsStackNavigator />
-//     </View>
-//   );
-// }
-
 
 export default function App() {
   return (
@@ -84,9 +56,7 @@ export default function App() {
       <Tab.Navigator
         initialRouteName="Home"
         screenOptions={({ route }) => ({
-          
           tabBarIcon: ({ focused, color, size }) => {
-            //adding icons
             let iconName;
             if (route.name === 'Info') {
               iconName = 'info';
@@ -94,18 +64,13 @@ export default function App() {
               iconName = 'home';
             } else if (route.name === 'Queue') {
               iconName = 'user';
-            } else if (route.name == 'Settings'){
+            } else if (route.name === 'Settings') {
               iconName = 'settings';
             }
             return <Feather name={iconName} size={size} color={focused ? 'red' : 'white'} />;
           },
-          tabBarLabelStyle: {
-            fontSize: 12,
-          },
-          tabBarStyle: {
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
-            borderTopWidth: 0,
-          },
+          tabBarLabelStyle: { fontSize: 12 },
+          tabBarStyle: { backgroundColor: 'rgba(0, 0, 0, 0.5)', borderTopWidth: 0 },
           tabBarActiveTintColor: '#910307',
           tabBarInactiveTintColor: 'white',
         })}
@@ -113,7 +78,7 @@ export default function App() {
         <Tab.Screen name="Info" component={InfoScreen} />
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Queue" component={QueueScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Settings" component={SettingsStackNavigator} />
       </Tab.Navigator>
     </NavigationContainer>
   );
