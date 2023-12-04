1edd506ebbdd0db5aec8840c41561e0b2088d47d
Riya Singh <caspriya9211@gmail.com>
2023-12-03 23:20:20 -0500

    Finished Notifications Settings option
    Navigation withing settings option is broken needs a pair of eyes

--- HackRPIEventApp2023/SettingsOptions/AccessibilitySettings.js
+++ HackRPIEventApp2023/SettingsOptions/AccessibilitySettings.js
@@ -1,62 +1,28 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity,Switch, Button } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const AccessibilityOptions = () => {
   const [colorBlindMode, setColorBlindMode] = useState(false);
-  const [notificationsEnabled, setNotificationsEnabled] = useState(true);
 
   const toggleColorBlindMode = () => {
     setColorBlindMode(!colorBlindMode);
     // Implement functionality to apply color blind mode
     // For example, you might apply a different color scheme
     // or modify colors in your app based on the colorBlindMode state
-
-  };
-
-  const toggleNotifications = () => {
-    setNotificationsEnabled(!notificationsEnabled);
-    // Implement functionality to manage notifications
-    // For example, you might use a notification library like react-native-push-notification
-  };
-
-  const silenceNotifications = () => {
-    // Code to silence notifications (e.g., using a library like react-native-push-notification)
-    console.log('Notifications silenced');
   };
 
   return (
     <View style={styles.container}>
       <Text>Accessibility Options</Text>
-      {/* Toggle Color Blind Mode */}
       <TouchableOpacity onPress={toggleColorBlindMode}>
         <Text style={styles.option}>
           Color Blind Mode: {colorBlindMode ? 'On' : 'Off'}
         </Text>
       </TouchableOpacity>
-      
-      {/* Toggle Notifications */}
-      <View style={styles.option}>
-        <Text>Enable Notifications</Text>
-        <Switch
-          value={notificationsEnabled}
-          onValueChange={toggleNotifications}
-        />
-      </View>
-      
-      {/* Silence Notifications */}
-      <View style={styles.option}>
-        <Button
-          title={notificationsEnabled ? 'Silence Notifications' : 'Unsilence Notifications'}
-          onPress={silenceNotifications}
-          disabled={!notificationsEnabled}
-        />
-      </View>
-      
       {/* Other suboptions and functionality here */}
     </View>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
@@ -68,12 +34,8 @@ const styles = StyleSheet.create({
   option: {
     fontSize: 16,
     marginTop: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    width: '80%',
+    color: '#333', // You can adjust the color as needed
   },
 });
-
 
 export default AccessibilityOptions;
--- HackRPIEventApp2023/SettingsOptions/NotificationSettings.js
+++ HackRPIEventApp2023/SettingsOptions/NotificationSettings.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Switch, Button } from 'react-native';
 
-const AccessibilityOptions = () => {
+const NotificationOptions = () => {
+  const [notificationsEnabled, setNotificationsEnabled] = useState(true);
+
+  const toggleNotifications = () => {
+    setNotificationsEnabled((prev) => !prev);
+    // Implement functionality to manage notifications
+    // For example, you might use a notification library like react-native-push-notification
+  };
+
+  const silenceNotifications = () => {
+    // Code to silence notifications (e.g., using a library like react-native-push-notification)
+    console.log('Notifications silenced');
+  };
+
   return (
     <View style={styles.container}>
       <Text>Notification Options</Text>
-      {/* Suboptions and functionality here */}
+       {/* Enable/Disable Notifications */}
+       <View style={styles.option}>
+        <Text>Enable Notifications</Text>
+        <Switch
+          value={notificationsEnabled}
+          onValueChange={toggleNotifications}
+        />
+      </View>
+      
+      {/* Silence Notifications */}
+      <View style={styles.option}>
+        <Button
+          title={notificationsEnabled ? 'Silence Notifications' : 'Unsilence Notifications'}
+          onPress={silenceNotifications}
+          disabled={!notificationsEnabled}
+        />
+      </View>
+      
+      {/* Other suboptions and functionality here */}
     </View>
   );
 };
@@ -18,4 +49,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  option: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '80%',
+    marginVertical: 10,
+  },
 });
+
+export default NotificationOptions;
