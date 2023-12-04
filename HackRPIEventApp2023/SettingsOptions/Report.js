3562d4f33884f39f971ec6375a13ec1b877a09b6
Riya Singh <caspriya9211@gmail.com>
2023-12-03 23:58:48 -0500

    Completed Bug Reporting
    needs some fixing

--- HackRPIEventApp2023/SettingsOptions/Report.js
+++ HackRPIEventApp2023/SettingsOptions/Report.js
@@ -1,12 +1,69 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState }from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 const Report = () => {
+  const [issueDescription, setIssueDescription] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = () => {
+    // Implement functionality to submit the report
+    // You can send the issueDescription to a server or perform actions accordingly
+    setIsSubmitted(true);
+    // ... additional logic to submit the report
+  };
+
   return (
-    <View>
-      <Text>Notification Settings Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Report an Issue</Text>
+
+      {!isSubmitted ? (
+        <>
+          {/* Issue Description Text Input */}
+          <TextInput
+            style={styles.input}
+            multiline
+            placeholder="Describe the issue..."
+            value={issueDescription}
+            onChangeText={setIssueDescription}
+          />
+
+          {/* Submit Button */}
+          <Button title="Submit Report" onPress={handleSubmit} />
+        </>
+      ) : (
+        <Text style={styles.confirmationText}>
+          The report has been submitted. We appreciate your patience. Thank you!
+        </Text>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    padding: 20,
+  },
+  heading: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+    width: '100%',
+    minHeight: 100,
+  },
+  confirmationText: {
+    textAlign: 'center',
+    fontSize: 18,
+  },
+});
 
 export default Report;
