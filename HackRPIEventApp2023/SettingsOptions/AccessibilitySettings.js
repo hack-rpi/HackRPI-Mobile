01a35fbfea3a4bbec103c0948f7675ae3f7c24e0
Adam Smart <adam.smart830@gmail.com>
2023-12-08 11:09:53 -0500

    Stylized Accessibility Settings Page

--- HackRPIEventApp2023/SettingsOptions/AccessibilitySettings.js
+++ HackRPIEventApp2023/SettingsOptions/AccessibilitySettings.js
@@ -7,46 +7,40 @@ const AccessibilityOptions = () => {
 
   const toggleColorBlindMode = () => {
     setColorBlindMode(!colorBlindMode);
-    // Implement functionality to apply color blind mode
-    // For example, you might apply a different color scheme
-    // or modify colors in your app based on the colorBlindMode state
+    // Additional functionality to apply color blind mode
   };
 
   const increaseFontSize = () => {
-    setFontSize(fontSize + 2); // Increase font size by 2 units (you can adjust this value)
+    setFontSize(fontSize + 2);
   };
 
   const decreaseFontSize = () => {
     if (fontSize > 2) {
-      setFontSize(fontSize - 2); // Decrease font size by 2 units (you can adjust this value)
+      setFontSize(fontSize - 2);
     }
   };
 
-
   return (
     <View style={styles.container}>
-      <Text>Accessibility Options</Text>
       {/* Toggle Color Blind Mode */}
-      <TouchableOpacity onPress={toggleColorBlindMode}>
-        <Text style={styles.option}>
+      <TouchableOpacity style={[styles.button, colorBlindMode ? styles.buttonActive : null]} onPress={toggleColorBlindMode}>
+        <Text style={[styles.buttonText, colorBlindMode ? styles.buttonTextActive : null]}>
           Color Blind Mode: {colorBlindMode ? 'On' : 'Off'}
         </Text>
       </TouchableOpacity>
       
       {/* Increase Font Size */}
-      <TouchableOpacity onPress={increaseFontSize}>
-        <Text style={styles.option}>Increase Font Size</Text>
+      <TouchableOpacity style={styles.button} onPress={increaseFontSize}>
+        <Text style={styles.buttonText}>Increase Font Size</Text>
       </TouchableOpacity>
       
       {/* Decrease Font Size */}
-      <TouchableOpacity onPress={decreaseFontSize}>
-        <Text style={styles.option}>Decrease Font Size</Text>
+      <TouchableOpacity style={styles.button} onPress={decreaseFontSize}>
+        <Text style={styles.buttonText}>Decrease Font Size</Text>
       </TouchableOpacity>
       
-      {/* Other suboptions and functionality here */}
-      
       {/* Example Text using the adjusted font size */}
-      <Text style={{ fontSize: fontSize, marginTop: 20 }}>
+      <Text style={{ fontSize: fontSize, marginTop: 20, color: '#333' }}>
         Example Text with Adjusted Font Size
       </Text>
     </View>
@@ -56,14 +50,29 @@ const AccessibilityOptions = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#000', // Background color similar to the image
   },
-  option: {
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    margin: 10,
+    backgroundColor: '#fff', // White background for the button
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  buttonText: {
     fontSize: 16,
-    marginTop: 10,
-    color: '#333', // You can adjust the color as needed
+    color: '#333', // Text color for buttons
+    fontWeight: 'bold', // Bold font weight as seen in the image
+  },
+  buttonActive: {
+    backgroundColor: '#f00', // Red background for the active state
+  },
+  buttonTextActive: {
+    color: '#fff', // White text color for the active state
   },
 });
 
