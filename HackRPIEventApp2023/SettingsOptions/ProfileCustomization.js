e17d443a736442e3de911f094f972729cf821c05
Adam Smart <adam.smart830@gmail.com>
2023-12-08 11:44:21 -0500

    stylized Profile Customization Page

--- HackRPIEventApp2023/SettingsOptions/ProfileCustomization.js
+++ HackRPIEventApp2023/SettingsOptions/ProfileCustomization.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet  } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet  } from 'react-native';
 
 const ProfileCustomization = () => {
   const [preferredName, setPreferredName] = useState('');
@@ -15,12 +15,13 @@ const ProfileCustomization = () => {
 
   return (
     <View style={styles.container}>
-      <Text>Profile Customization</Text>
+      <Text style={styles.title}>Profile Customization</Text>
 
       {/* Preferred Name */}
       <TextInput
         style={styles.input}
         placeholder="Preferred Name"
+        placeholderTextColor="#666" // Placeholder text color
         value={preferredName}
         onChangeText={setPreferredName}
       />
@@ -29,6 +30,7 @@ const ProfileCustomization = () => {
       <TextInput
         style={styles.input}
         placeholder="School Name"
+        placeholderTextColor="#666"
         value={schoolName}
         onChangeText={setSchoolName}
       />
@@ -37,6 +39,7 @@ const ProfileCustomization = () => {
       <TextInput
         style={styles.input}
         placeholder="Major"
+        placeholderTextColor="#666"
         value={major}
         onChangeText={setMajor}
       />
@@ -44,14 +47,17 @@ const ProfileCustomization = () => {
       {/* Years of Experience */}
       <TextInput
         style={styles.input}
-        placeholder="Years of Experience in Hacking"
+        placeholder="Years of Experience"
+        placeholderTextColor="#666"
         value={yearsOfExperience}
         onChangeText={setYearsOfExperience}
         keyboardType="numeric"
       />
 
       {/* Save Button */}
-      <Button title="Save Changes" onPress={handleSave} />
+      <TouchableOpacity style={styles.button} onPress={handleSave}>
+        <Text style={styles.buttonText}>Save Changes</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -61,16 +67,36 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#FFFFFF',
+    backgroundColor: '#000', // Changed to black to match the theme
     padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff', // Changed to white to match the theme
+    marginBottom: 20,
   },
   input: {
     borderWidth: 1,
-    borderColor: '#CCCCCC',
-    borderRadius: 5,
+    borderColor: '#fff', // Border color changed to white
+    borderRadius: 20, // Rounded corners to match the theme
     padding: 10,
     marginBottom: 10,
     width: '100%',
+    color: '#fff', // Text color changed to white
+    backgroundColor: '#333', // Darker background for the input
+  },
+  button: {
+    backgroundColor: '#f00', // Red background for the button
+    borderRadius: 20, // Rounded corners for the button
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#fff', // White text color for the button
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
 
