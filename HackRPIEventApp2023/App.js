d3f08ad7a1015c1f61d8534888749bb0985affd0
kesfc <kevinli20030305@gmail.com>
2024-02-07 18:56:15 -0500

    login page first draft done

--- HackRPIEventApp2023/App.js
+++ HackRPIEventApp2023/App.js
@@ -1,45 +1,9 @@
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import Timer from './components/Timer';
+// App.js
+import React from 'react';
+import Login from './Login';
 
-const Circle = ({ size, percentage }) => {
-  return (
-    <View style={[styles.circle, { width: size, height: size }]}>
-      <Timer percentage={percentage} circleWidth={size.toString()} />
-    </View>
-  );
+const App = () => {
+  return <Login />;
 };
 
-export default function App() {
-  const [percentage, setPercentage] = useState(35);
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.circleContainer}>
-        <Circle size={100} percentage={percentage} />
-        <Timer percentage={percentage} circleWidth="200" />
-        <Circle size={100} percentage={percentage} />
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  circleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-  },
-  circle: {
-    borderRadius: 50,
-    overflow: 'hidden',
-  },
-});
+export default App;
