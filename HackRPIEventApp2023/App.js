0999bddf0a6b1b094e06151e7f62823b70cab1f9
kesfc <kevinli20030305@gmail.com>
2024-02-09 16:39:52 -0500

    make a link from home to login page

--- HackRPIEventApp2023/App.js
+++ HackRPIEventApp2023/App.js
@@ -1,9 +1,36 @@
-// App.js
-import React from 'react';
-import Login from './Login';
+import React, {useState} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Button, View, StyleSheet } from 'react-native';
+import Home from './LoginHome';
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  return <Login />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
+
+const MainScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;
