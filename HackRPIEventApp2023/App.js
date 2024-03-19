f42ca0edd0f1da323fddc534106fde7a41617f4c
TouHouPeace <143843739+TouHouPeace@users.noreply.github.com>
2024-03-18 23:49:12 -0400

    test

--- HackRPIEventApp2023/App.js
+++ HackRPIEventApp2023/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Feather } from "@expo/vector-icons";
@@ -7,7 +7,6 @@ import { StatusBar } from "expo-status-bar";
 import Calander from "./Box/Calander";
 import Food from "./information/Food";
 import HackerQue from "./HackerQue/QueEntry.js";
-// import { colors } from './colors';
 import { globalStyles } from "./styles";
 
 const Tab = createBottomTabNavigator();
@@ -24,8 +23,6 @@ function HomeScreen() {
   return (
     <View style={styles.container}>
       <Calander />
-
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -41,40 +38,42 @@ function QueueScreen() {
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            //adding icons
-            let iconName;
-            if (route.name === "Info") {
-              iconName = "info";
-            } else if (route.name === "Home") {
-              iconName = "home";
-            } else if (route.name === "Queue") {
-              iconName = "user";
-            }
-            return (
-              <Feather
-                name={iconName}
-                size={size}
-                color={focused ? "red" : "white"}
-              />
-            );
-          },
-          tabBarLabelStyle: {
-            fontSize: 12,
-          },
-          tabBarStyle: {
-            backgroundColor: globalStyles.primary, // should this be transparent?
-            borderTopWidth: 0, // Hide top border of the tab bar
-          },
-          tabBarActiveTintColor: globalStyles.accent,
-          tabBarInactiveTintColor: "white",
-        })}>
-        <Tab.Screen name="Info" component={InfoScreen} />
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Queue" component={QueueScreen} />
-      </Tab.Navigator>
+      <View style={styles.container}>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              if (route.name === "Info") {
+                iconName = "info";
+              } else if (route.name === "Home") {
+                iconName = "home";
+              } else if (route.name === "Queue") {
+                iconName = "user";
+              }
+              return (
+                <Feather
+                  name={iconName}
+                  size={size}
+                  color={focused ? "red" : "white"}
+                />
+              );
+            },
+            tabBarLabelStyle: {
+              fontSize: 12,
+            },
+            tabBarStyle: {
+              backgroundColor: globalStyles.primary,
+              borderTopWidth: 0,
+            },
+            tabBarActiveTintColor: globalStyles.accent,
+            tabBarInactiveTintColor: "white",
+          })}>
+          <Tab.Screen name="Info" component={InfoScreen} />
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Queue" component={QueueScreen} />
+        </Tab.Navigator>
+        <StatusBar style="light" />
+      </View>
     </NavigationContainer>
   );
 }
@@ -83,21 +82,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: globalStyles.primary,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: globalStyles.fontSize,
-    fontWeight: globalStyles.fontWeight,
-    color: globalStyles.text,
-  },
-  circleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-around",
-  },
-  circle: {
-    borderRadius: 50,
-    overflow: "hidden",
   },
 });
