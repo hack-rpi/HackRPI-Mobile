451bd5d95d4f4d36a9ada5c1356dc4354fdb0374
LuCaS9798 <83709408+LuCaS9798@users.noreply.github.com>
2024-04-23 22:31:56 -0400

    FAQ_styles

--- HackRPIEventApp2023/styles.js
+++ HackRPIEventApp2023/styles.js
@@ -9,4 +9,38 @@ export const globalStyles = {
   //primary, secondary, accent, accent2, text
   fontSize: 20,
   fontWeight: "bold",
-};
+
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 20,
+    backgroundColor: '#222',
+  },
+  headerText: {
+    fontSize: 24,
+    color: 'white',
+  },
+  faqItem: {
+    margin: 10,
+    padding: 10,
+    backgroundColor: '#444',
+    borderRadius: 5,
+  },
+  question: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    alignItems: 'center',
+  },
+  questionText: {
+    color: 'white',
+  },
+  answer: {
+    padding: 10,
+    color: 'white',
+  },
+}
