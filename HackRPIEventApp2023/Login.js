d6957981d59352f140c0d25011f4475ef4927d48
LuCaS9798 <83709408+LuCaS9798@users.noreply.github.com>
2024-04-24 00:21:10 -0400

    Update Login.js

--- HackRPIEventApp2023/Login.js
+++ HackRPIEventApp2023/Login.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, Animated, StyleSheet } from 'react-native';
 
 const LoginPage = ({ navigation }) => {
+  const [isCheckingAuth, setIsCheckingAuth] = useState(true);
+  const scaleValue = new Animated.Value(1);
+
+  useEffect(() => {
+    // Simulate checking for token or user authentication status
+    setTimeout(() => {
+      setIsCheckingAuth(false);
+    }, 2000);
+  }, []);
+
+  const navigateToScreen = (screen) => () => {
+    Animated.sequence([
+      Animated.timing(scaleValue, { toValue: 0.95, duration: 150, useNativeDriver: true }),
+      Animated.timing(scaleValue, { toValue: 1, duration: 150, useNativeDriver: true }),
+    ]).start(() => {
+      navigation.navigate(screen);
+    });
+  };
+
+  if (isCheckingAuth) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Checking authentication...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.welcomeText}>Welcome to HackRPI!</Text>
-      {/* Other UI elements */}
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('MentorMainApp')}>
-        <Text style={styles.buttonText}>Login as Mentor</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('HackerMainApp')}>
-        <Text style={styles.buttonText}>Continue as Hacker</Text>
-      </TouchableOpacity>
+      <Text style={styles.text}>Welcome to HackRPI!</Text>
+      <AnimatedButton title="Login as Mentor" onPress={navigateToScreen('MentorMainApp')} scaleValue={scaleValue} />
+      <AnimatedButton title="Continue as Hacker" onPress={navigateToScreen('HackerMainApp')} scaleValue={scaleValue} />
     </View>
   );
 };
+
+const AnimatedButton = ({ title, onPress, scaleValue }) => (
+  <Animated.View style={{ transform: [{ scale: scaleValue }] }}>
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  </Animated.View>
+);
 
 
 const styles = StyleSheet.create({
